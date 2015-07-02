e93bc1c94e6cb82b46fe6b8ac8741b38998314b1
gregoryfabry <glfabry2@illinois.edu>
2015-07-02 15:57:59 -0500

    tuioooooooooooooooo

--- client.js
+++ client.js
@@ -19,18 +19,25 @@ var config = fs.existsSync(path.resolve(__dirname, 'client.json')) ? JSON.parse(
     "udp": 8088
   },
   "server": "localhost"
-}
+};
+
+var tuio = new (require('caress-server'))('0.0.0.0', 3333, {'json': true});
+var ipc = require('ipc')
+
+tuio.on('tuio', function(data) {
+  ipc.send('tuio', data);
+});
 
 // This method will be called when Electron has done everything
 // initialization and ready for creating browser windows.
 app.on('ready', function() {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    'always-on-top': true,
-    'resizeable': false,
-    'fullscreen': true,
-    'frame': false,
-    'kiosk': true
+   // 'always-on-top': true,
+   // 'resizeable': false,
+   // 'fullscreen': true,
+   // 'frame': false,
+   // 'kiosk': true
   });
   mainWindow.on('page-title-updated', function(e) {
     mainWindow.close();
@@ -39,7 +46,7 @@ app.on('ready', function() {
   var dgram = require('dgram');
   var socket = dgram.createSocket('udp4');
   var loaded = false;
-  
+
   socket.on('message', function(buf, info) {
     var message = JSON.parse(buf.toString());
     if (!loaded && 'ports' in message) {
@@ -47,7 +54,7 @@ app.on('ready', function() {
       loaded = true;
     }
   });
-  
+
   socket.bind(config.ports.udp);
   socket.on('listening', function() {
     socket.setBroadcast(true);
@@ -57,7 +64,7 @@ app.on('ready', function() {
     if (!loaded) {
       mainWindow.loadUrl('http://' + config.server + ':' + config.ports.http + '/?port=' + config.ports.ws + '&electron=1');
     }
-  }, 10000);
+  }, 0000);
 
   mainWindow.on('closed', function() {
     mainWindow = null;
